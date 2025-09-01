86d044b6ad8843c062608f4c7ec9f37a0a73b195
Syed Fahad Ali <155745867+syedfahadali399@users.noreply.github.com>
2025-09-02 02:05:52 +0500

    Update java.js

--- java.js
+++ java.js
@@ -102,4 +102,23 @@ getRange.addEventListener("input", (e) => {
 
     showLength.textContent = convertvalue
         
+
 })
+
+// let uppercaseLetters = [
+//     "A","B","C","D","E","F","G","H","I","J","K","L","M",
+//     "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+// ]; 
+
+// let lowercaseLetters = [
+//     "a","b","c","d","e","f","g","h","i","j","k","l","m",
+//     "n","o","p","q","r","s","t","u","v","w","x","y","z"
+// ];
+
+// let numbers = ["0","1","2","3","4","5","6","7","8","9"];
+
+// let symbols = [
+//     "!","@","#","$","%","^","&","*","(",")",
+//     "_","+","[","]","{","}","|",";",":",",",".",
+//     "<",">","?","/"
+// ];
